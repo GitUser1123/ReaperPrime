96724a71349ef9522d42b6c58df76d9959d5428c
mtrlt <mtrlt@mtrlt.com>
2013-07-26 06:41:16 +0300

    Fixed block aux data generation.
    Now 50% of blocks won't be invalid.

--- CPUAlgos_global.cpp
+++ CPUAlgos_global.cpp
@@ -4,20 +4,15 @@
 
 vector<uchar> XPM_create_auxdata(mpz_t* bnChainOrigin)
 {
-	size_t exportsz, resultoff;
+	size_t exportsz;
 	uchar* ex_port = (uchar*)mpz_export(NULL, &exportsz, -1, 1, -1, 0, *bnChainOrigin);
 	//assert(exportsz < 250);  // FIXME: bitcoin varint
-	resultoff = 1;
-	if (ex_port[0] & 0x80)
-		++resultoff;
-	uchar* result = new uchar[exportsz+resultoff];
-	result[0] = exportsz + resultoff - 1;
+	uchar* result = new uchar[exportsz+1];
+	result[0] = exportsz;
 	result[1] = '\0';
-	memcpy(&result[resultoff], ex_port, exportsz);
-	if (mpz_sgn(*bnChainOrigin) < 0)
-		result[1] |= 0x80;
+	memcpy(&result[1], ex_port, exportsz);
 	free(ex_port);
-	vector<uchar> vec(result,result+exportsz+resultoff);
+	vector<uchar> vec(result,result+exportsz+1);
 	delete[]result;
 	return vec;
 }
